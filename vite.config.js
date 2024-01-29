7c1f6909c6da5c013fd15db7a0075f1a5a0691d5
David Rios <david@davidrios.co>
2024-01-29 09:38:44 -0500

    fix: Multiple fixes
    - Added "gracias.html" file and its entry on vite.config.js
    - File "main.ts" is now called "script.js"
    - Transpilation is now made using relative paths
    - Deleted unnecessary files on "public" directory

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // gracias: resolve(__dirname, 'gracias.html'),
+        gracias: resolve(__dirname, 'gracias.html'),
       },
     },
   },
